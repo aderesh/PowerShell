76091acf62b64244374fa1fcfc3b7e26b01c037b
aderesh <andryakaad@gmail.com>
2021-03-06 16:14:37 -0500

    add gray foreground for path

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,8 @@
 function prompt 
   { 
     $host.ui.rawui.WindowTitle = "$((Get-Item -Path $pwd).Name) | $pwd";
+	Write-Host $pwd -foreground gray;
     Write-Host ">" -foreground red -nonewline;
     return " ";
   }
+
